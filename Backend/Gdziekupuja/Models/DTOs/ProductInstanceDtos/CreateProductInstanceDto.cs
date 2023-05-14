b55b6fddc66830d80e6131e658a069538dd47459
PanJan44 <jasjanek33@gmail.com>
2023-05-14 14:35:04 +0200

    Tablica zamiast ICollection w Dto instancji produktu

--- Backend/Gdziekupuja/Models/DTOs/ProductInstanceDtos/CreateProductInstanceDto.cs
+++ Backend/Gdziekupuja/Models/DTOs/ProductInstanceDtos/CreateProductInstanceDto.cs
@@ -1,11 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+
 namespace Gdziekupuja.Models.DTOs.ProductInstanceDtos;
 
 public class CreateProductInstanceDto
 {
     [Required] public int ProductId { get; set; }
 
-    [Required] public ICollection<int> CategoryIds { get; set; } = null!;
+    [Required] public int[] CategoryIds { get; set; } = null!;
 
     public IDictionary<string, string> AdditionalInfo { get; set; } = null!;
 
