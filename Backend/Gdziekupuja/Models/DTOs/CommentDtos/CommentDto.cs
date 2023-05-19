f2a79c0138e4f08fc845caafd01068496c6b140c
matebab551 <mat620.mati2@interia.pl>
2023-05-19 21:19:15 +0200

    ilość like i dislike

--- Backend/Gdziekupuja/Models/DTOs/CommentDtos/CommentDto.cs
+++ Backend/Gdziekupuja/Models/DTOs/CommentDtos/CommentDto.cs
@@ -3,8 +3,8 @@ namespace Gdziekupuja.Models.DTOs.CommentDtos;
 public class CommentDto
 {
     public int Id { get; set; }
-    public int Likes { get; set; }
-    public int DisLikes { get; set; }
+    public int Likers { get; set; }
+    public int DisLikers { get; set; }
     public string Content { get; set; } = null!;
     public string Author { get; set; } = null!;
     public int AuthorId { get; set; }
